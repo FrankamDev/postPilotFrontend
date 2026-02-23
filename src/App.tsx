054b604417371f141850673cc6147ea2fdee33d6
FrankamDev <fkamgang06@gmail.com>
2026-02-23 09:52:54 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="text-2xl font-bold text-blue-300 text-center">je ne sais pas comment faire</div>
+    </>
+  )
+}
+
+export default App
